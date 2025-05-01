f24b0fb9165eb014356d6d0ab9fa02ad267c9a72
wailman24 <wailmansour24@gmail.com>
2025-05-01 16:32:25 +0100

    fix vercel errors

--- src/pages/dashboardseller/sellerdahsboard.tsx
+++ src/pages/dashboardseller/sellerdahsboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+//import React from "react";
 import { Card, CardContent } from "../../components/ui/card";
 import { BarChart2, Package, ShoppingCart, DollarSign } from "lucide-react";
 
